621555aedc1f5dabc1da1e2f29a62c5933c3efef
mateuszzieba <mz@cvt.services>
2023-10-26 09:49:28 +0200

    cs

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -1,13 +1,9 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { getProducts } from '../clients/productClient'
-import { ProductQueryResponse} from "../types/product";
 import { createOrder } from "../clients/orderClient";
 
 export const useOrderCrud = () => {
 
     const queryClient = useQueryClient();
-
-    const { data } = useQuery<ProductQueryResponse>('products', getProducts);
 
     const createOrderMutation = useMutation(createOrder,
         {
@@ -17,7 +13,6 @@ export const useOrderCrud = () => {
         });
 
     return {
-        products: data ? data.results : [],
         createOrder: createOrderMutation.mutate,
 
     };
